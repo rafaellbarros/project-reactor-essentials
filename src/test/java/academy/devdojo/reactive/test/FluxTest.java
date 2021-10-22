d609f0372f771f14823e6ffa7a8ad13ce8ede18f
Rafael Barros <rafaelbarros.df@gmail.com>
2021-10-22 00:13:46 -0300

    :construction: Backpressure on Flux publisher with BaseSubscriber

--- src/test/java/academy/devdojo/reactive/test/FluxTest.java
+++ src/test/java/academy/devdojo/reactive/test/FluxTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -112,6 +113,37 @@ public class FluxTest {
 
         StepVerifier.create(flux)
                 .expectNext(1, 2, 3,4,5,6,7,8,9,10)
+                .verifyComplete();
+    }
+
+    @Test
+    public void fluxSubscriberNumbersNotSoUglyBackpressure() {
+        Flux<Integer> flux = Flux.range(1, 10)
+                .log();
+
+        flux.subscribe(new BaseSubscriber<>() {
+            private int count = 0;
+            private final int requestCount = 2;
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(requestCount);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                count++;
+                if (count >= requestCount) {
+                    count = 0;
+                    request(requestCount);
+                }
+            }
+        });
+
+        log.info("-----------------------------------");
+
+        StepVerifier.create(flux)
+                .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
                 .verifyComplete();
     }
 }
